6a1b578b9b9a6d0b20e5bf62a1d612eac71b3c74
tyler <tyler@cruftbusters.com>
2021-03-06 11:42:48 -0600

    inverted horizontally adjacent triangles

--- src/Wallpaper.js
+++ src/Wallpaper.js
@@ -2,6 +2,7 @@ function Wallpaper() {
   return (
     <svg style={{ width: '100%', height: '100%' }}>
       <TriCell length={50} x={0} fill='red' />
+      <TriCell length={50} x={-1} fill='green' />
       <TriCell length={50} x={-2} fill='blue' />
     </svg>
   )
@@ -9,11 +10,24 @@ function Wallpaper() {
 
 function TriCell({ length, x, ...props }) {
   const { innerWidth: width, innerHeight: height } = window
-  const points = [
-    [width / 2 + (length * x) / 2, height - (length * Math.sqrt(3)) / 2],
-    [width / 2 + length / 2 + (length * x) / 2, height],
-    [width / 2 - length / 2 + (length * x) / 2, height],
-  ]
+  const isInverted = x % 2 != 0
+  const points = isInverted
+    ? [
+        [width / 2 + (length * x) / 2, height],
+        [
+          width / 2 + length / 2 + (length * x) / 2,
+          height - (length * Math.sqrt(3)) / 2,
+        ],
+        [
+          width / 2 - length / 2 + (length * x) / 2,
+          height - (length * Math.sqrt(3)) / 2,
+        ],
+      ]
+    : [
+        [width / 2 + (length * x) / 2, height - (length * Math.sqrt(3)) / 2],
+        [width / 2 + length / 2 + (length * x) / 2, height],
+        [width / 2 - length / 2 + (length * x) / 2, height],
+      ]
 
   return (
     <polygon
