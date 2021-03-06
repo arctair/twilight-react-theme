2b1c65c7e434804303fecb9fcf83549cf2d45960
tyler <tyler@cruftbusters.com>
2021-03-06 11:08:08 -0600

    github link

---
+++ src/GithubLink.js
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+
+function GithubLink() {
+  const [hover, setHover] = useState(false)
+  return (
+    <>
+      <a
+        href='https://github.com/arctair/twilight-react-theme'
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          right: 0,
+          margin: '0.5em',
+          textDecoration: 'none',
+          fontSize: '1.5em',
+          color: 'white',
+          ...(hover && {
+            textShadow: '0 0 3px white',
+          }),
+        }}
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+      >
+        Github
+      </a>
+    </>
+  )
+}
+
+export default GithubLink
