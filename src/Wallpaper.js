ec2db1f82a1bf941b1be052ddc6da83a3c89077d
tyler <tyler@cruftbusters.com>
2021-03-06 11:41:08 -0600

    non-inverted horizontally adjacent triangles

--- src/Wallpaper.js
+++ src/Wallpaper.js
@@ -1,22 +1,23 @@
 function Wallpaper() {
   return (
     <svg style={{ width: '100%', height: '100%' }}>
-      <TriCell length={50} fill='red' />
+      <TriCell length={50} x={0} fill='red' />
+      <TriCell length={50} x={-2} fill='blue' />
     </svg>
   )
 }
 
-function TriCell({ length, ...props }) {
+function TriCell({ length, x, ...props }) {
   const { innerWidth: width, innerHeight: height } = window
+  const points = [
+    [width / 2 + (length * x) / 2, height - (length * Math.sqrt(3)) / 2],
+    [width / 2 + length / 2 + (length * x) / 2, height],
+    [width / 2 - length / 2 + (length * x) / 2, height],
+  ]
+
   return (
     <polygon
-      points={[
-        [width / 2, height - (length * Math.sqrt(3)) / 2],
-        [width / 2 + length / 2, height],
-        [width / 2 - length / 2, height],
-      ]
-        .map((point) => point.join(','))
-        .join(' ')}
+      points={points.map((point) => point.join(',')).join(' ')}
       {...props}
     />
   )
